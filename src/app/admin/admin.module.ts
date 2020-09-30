222485350036b5acad27450c708365f24db1c314
PavelK6896 <79208776896@yandex.ru>
2020-09-30 12:17:25 +0300

    "add laze load module to route"

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,39 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
+
+
+// laze load module for admin
+@NgModule({
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent
+  ],
+  imports: [
+    CommonModule, // add all module
+    RouterModule.forChild([ // child module for route
+      {
+        //display and page on
+        path: '', component: AdminLayoutComponent, children: [
+          {path: '', redirectTo: '/admin/login', pathMatch: 'full'},//1
+          {path: 'login', component: LoginPageComponent},//1
+          {path: 'dashboard', component: DashboardPageComponent},//2
+          {path: 'create', component: CreatePageComponent},//3
+          {path: 'post/:id/edit', component: EditPageComponent}//4
+        ]
+      }
+    ])
+  ],
+  exports: [RouterModule] // экспортируем модули
+})
+export class AdminModule {
+
+}
